6366966ba3338d91c318e96af873eefefeae292f
deniselaroze <deniselaroze@gmail.com>
2018-07-09 13:30:10 -0400

    update

--- rscript/MergeFilesSP.R
+++ rscript/MergeFilesSP.R
@@ -53,17 +53,16 @@ table(table(all.files$perfil))
 ##Agrego ID Unico = Grupo + Perfil
 all.files <- cbind(id = paste0( all.files$grupo,".", all.files$perfil,".", all.files$pid),all.files )
 
-##Cambio ID a variable Categorica
-
+##Cambio ID a variable Categorica, para hacer pruebas
 all.files$nid<-as.numeric(all.files$id)
 table(all.files$nid)
 
-#str(all.files)
-#names(all.files)
-#numerize <- names(all.files)[c(1)]
-#for(nu in numerize){
-#  all.files[,nu]<- as.numeric(all.files[ ,nu])
-#}
+
+
+## Transformación de pensión en pesos (valor UF 27.161,48 01/07/2018)
+all.files$val_uf_pension_bru<-as.numeric(all.files$val_uf_pension_bru) #make numeric
+all.files$val_pesos_pension_bru<-all.files$val_uf_pension_bru*27161.48
+
 
 ## Genero nuevo archivo con los cambios
 write.table(all.files, "nuevaBD.csv", sep = ";", quote = F, row.names = T)
@@ -78,18 +77,92 @@ names(tbl)<-c("Valor Pensión", "Razon Social", "Clasificación de Riesgo")
 tbl<-xtable(tbl, caption="Leyenda del Control" )
 print(tbl, type="HTML", file="Tratamientos/control.html", include.rownames=FALSE  )
   
+###########################
+## Function - Control
+###########################
 
-## Function
 
 fcn.control <- function(gender, econ, mode, pair){
   id<-paste0(gender, econ, ".", mode, ".", pair)
   tbl<-all.files[all.files$id==id, c("razon_social", "val_uf_pension_bru", "riesgo")]
-  names(tbl)<-c( "Razón Social", "Valor Pensión", "Clasificación de Riesgo")
+  names(tbl)<-c( "Razón Social", "Valor Pensión UF", "Clasificación de Riesgo")
   tbl<-xtable(tbl, caption="Leyenda del Control" )
   return(print(tbl, type="HTML", file=paste0("Tratamientos/control", id ,".html"), include.rownames=FALSE  ))
 }
 
 
-
 fcn.control("F", "nivel2", "1a_RVI_simple", "co_1a_rp" )
+
+##########################
+### Function - Treatment 1
+##########################
+
+fcn.treat1 <- function(gender, econ, mode, pair){
+  id<-paste0(gender, econ, ".", mode, ".", pair)
+  tbl<-all.files[all.files$id==id, c("razon_social", "val_pesos_pension_bru", "riesgo")]
+  names(tbl)<-c( "Razón Social", "Valor Pensión $", "Clasificación de Riesgo")
+  tbl<-xtable(tbl, caption="Leyenda del Tratamiento 1" )
+  return(print(tbl, type="HTML", file=paste0("Tratamientos/Treat1", id ,".html"), include.rownames=FALSE  ))
+}
+
+
+fcn.treat1("F", "nivel2", "1a_RVI_simple", "co_1a_rp" )
+
+
+
+
+
+
+##########################
+### Function - Treatment 4 // Simulation code, this still needs to be adjusted and tested using real data
+##########################
+
+n<-12
+
+
+df$Company <- factor(df$Company, levels = df$Company[rev(order(df$VPE))])
+max<-max(df$VPE)+450000
+min<-min(df$VPE)-500000
+
+uf<-max(df$PensionUF)
+uf<-format(uf, decimal.mark = ",", scientific = FALSE)
+vpem<-max(df$VPE_mensual)
+vpem<-format(vpem, big.mark = ".", scientific = FALSE)
+
+
+chart_note0 <- paste("Oferta ", df$Company[df$VPE_mensual==max(df$VPE_mensual)], ": " , uf," UF mensuales",sep="")
+chart_note2 <- paste(vpem," pesos",sep="")
+chart_note1 <- paste(uf," UF",sep="")
+
+point <- format_format(big.mark = ".", decimal.mark = ",", scientific = FALSE)
+
+options(scipen=1000)
+p<-ggplot(data=df, aes(x=Company, y=VPE, fill = Company)) + 
+  geom_bar(stat="identity") +
+  #geom_text(aes(label=VPE_mensual, vjust=-0.8)) +
+  scale_fill_manual(values= rev(colorRampPalette(brewer.pal(11, "RdYlGn"))(n))) +
+  theme(legend.position="") +
+  scale_y_continuous(labels=function(x) format(x, big.mark = ".",decimal.mark=",",
+                                               scientific = FALSE)#,
+                     #                    sec.axis = sec_axis(~./240, name = "Pensión Mensual (pesos)", labels=function(x) format(x, big.mark = ".", decimal.mark = ",", scientific = FALSE))
+  )+
+  ylab("Total Valor Económico Pensión")  + xlab("")  +
+  theme(axis.text.y=element_text(size=rel(1.4) , angle=90),
+        axis.title.y=element_text(size=rel(1.8) ),
+        axis.text.x=element_text(size=rel(1.4), angle=90),
+        panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(colour = "Grey30", linetype = "dashed"))+
+  geom_text(aes(label = paste0("$",point(pensionPesos)) , angle=90, size = 2, vjust = 0.4, hjust= -0.1)) +
+  coord_cartesian(ylim=c(min,max))  #coord_flip() +
+
+p 
+
+ggsave("Treatment_4c.png", width=30, height = 15, units = "cm")
+
+
+
+
+
+
+
 
